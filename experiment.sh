c176082e5b52105be045c0e5157797c8eccf94d3
littlepig2013 <zhuzichen19960417@gmail.com>
2018-01-13 10:02:42 +0800

    experiment shell k updated

--- experiment.sh
+++ experiment.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+k=5
 experiment_type="classifier"
 datasets=( "DBLP" )
 penalty_types=( 1 2 3 4 )
@@ -16,10 +16,10 @@ do
 	do
 		for penalty_type in "${penalty_types[@]}"
 		do
-			output_file="output/"$experiment_type"_"$dataset"_"$penalty_type"_"$tfidf_type"_5.txt"
-		 	./topKQueryTest --"$experiment_type" "$dataset" "$dataset"_pos_pairs.txt $target_file 5 $penalty_type $tfidf_type > $output_file
+			output_file="output/"$experiment_type"_"$dataset"_"$penalty_type"_"$tfidf_type"_"$k".txt"
+		 	./topKQueryTest --"$experiment_type" "$dataset" "$dataset"_pos_pairs.txt $target_file $k $penalty_type $tfidf_type > $output_file
 		done
 	done
-	./topKQueryTest --"$experiment_type" "$dataset" "$dataset"_pos_pairs.txt $target_file 5 1 SP > "output/"$experiment_type"_"$dataset"_SP_5.txt"
+	./topKQueryTest --"$experiment_type" "$dataset" "$dataset"_pos_pairs.txt $target_file $k 1 SP > "output/"$experiment_type"_"$dataset"_SP_"$k".txt"
 done
 
