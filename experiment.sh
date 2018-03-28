516591b816d680912f3c08f43a84f31534861127
littlepig2013 <zhuzichen19960417@gmail.com>
2018-03-28 15:42:57 +0800

    experiment init

--- experiment.sh
+++ experiment.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 k=5
-experiment_type="classifier"
-datasets=( "DBLP" )
-betas=( "0.2" )
+experiment_type="recommend"
+datasets=( "ACM" )
+betas=( "0.3" )
 #betas=( "0.1" )
 tfidf_types=("S-M-S" "S-B-S" "M-S" "B-S" ) 
 target_file="labels.txt"
@@ -19,11 +19,11 @@ do
 		for beta in "${betas[@]}"
 		do
 			output_file=$outDir$experiment_type"_"$dataset"_1_"$tfidf_type"_"$beta"_"$k".txt"
-		 	./topKQueryTest --"$experiment_type" "$dataset" "$dataset"_pos_pairs.txt $target_file $k $tfidf_type 1 $beta > $output_file
+		 	#./topKQueryTest --"$experiment_type" "$dataset" "$dataset"_pos_pairs.txt $target_file $k $tfidf_type 1 $beta > $output_file
 		done
 
 		output_file=$outDir$experiment_type"_"$dataset"_2_"$tfidf_type"_"$k".txt"
-		./topKQueryTest --"$experiment_type" "$dataset" "$dataset"_pos_pairs.txt $target_file $k $tfidf_type 2 > $output_file
+		#./topKQueryTest --"$experiment_type" "$dataset" "$dataset"_pos_pairs.txt $target_file $k $tfidf_type 2 > $output_file
 	done
 	#./topKQueryTest --"$experiment_type" "$dataset" "$dataset"_pos_pairs.txt $target_file $k SP 1 > $outDir$experiment_type"_"$dataset"_SP_"$k".txt"
 	./topKQueryTest --"$experiment_type" "$dataset" "$dataset"_pos_pairs.txt $target_file $k SLV1 1 > $outDir$experiment_type"_"$dataset"_SLV1_"$k".txt"
