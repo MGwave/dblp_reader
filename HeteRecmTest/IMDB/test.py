7ac04634386b8d17311ee15455d4b92c8f57b603
littlepig2013 <zhuzichen19960417@gmail.com>
2018-04-03 14:47:31 +0800

    HeteRecom Test 1st

--- HeteRecmTest/IMDB/test.py
+++ HeteRecmTest/IMDB/test.py
@@ -1,110 +1,133 @@
 from HIN import EntityInfo
 from Similarity import *
-import pickle, random, math, time
+import os, pickle, random, math, time
+
+alpha = 0.5
+
+#loading the edge type file and the strength file
+def loadEdgeTypeStrength():
+    fin = open('../../IMDB/IMDBEdgeType.txt','r')
+    fin2 = open('../../cache/IMDBedgeTypeAvgDegree.txt','r')
+    edgeTypeDict = dict()
+    edgeTypeRecords = fin.readlines()
+    for record in edgeTypeRecords:
+        [edgeTypeStr, edgeTypeIdStr] = record.strip().split("\t")
+        edgeTypeDict[int(edgeTypeStr)] = edgeTypeStr
+    fin.close()
+
+    edgeTypeStrength = dict()
+    edgeTypeStrengthRecords = fin2.readlines()
+    for record in edgeTypeStrengthRecords:
+        [edgeTypeIdStr, inputStrengthStr, outputStrengthStr] = record.split("\t")
+        edgeTypeStrength[int(edgeTypeIdStr)] = 1.0/math.pow(float(inputStrengthStr)*float(outputStrengthStr), alpha)
+    fin2.close()
+    return edgeTypeDict, edgeTypeStrength
+
+# transfrom a edge type-based meta path to a node type based meta path
+def transform(metaPath, edgeTypeDict):
+    nodeTypeMetaPath = []
+    pathLength = len(metaPath)
+    for i in (0, pathLength):
+        edgeType = metaPath[i]
+        nodeType = []
+        if edgeType > 0:
+            nodeType = edgeTypeDict[edgeType].strip().split("_")
+        else:
+            nodeType = edgeTypeDict[edgeType].strip().split("_")
+            nodeType.reverse()
+
+        nodeTypeMetaPath.append(nodeType[0])
+        if i == pathLength - 1:
+            nodeTypeMetaPath.append(nodeType[1])
+    return nodeTypeMetaPath
+
+
+#loading the topK meta path for the given src and dst entity, the k and the method
+def loadTopKMetaPath(src, dst, method, k=5, edgeTypeDict):
+    filename = "../../topKResult/IMDB_" + method + "_" + str(src) + "_" + str(dst) + "_" + str(k) + ".txt"
+    fin = open(filename, "r")
+    topKMetaPathsRecords = fin.readlines()
+    metaPaths = []
+    for record in topKMetaPathsRecords:
+        tmpMetaPathStr = record.strip().split("\t")
+        tmpMetaPath = [int(edgeTypeStr) for edgeTypeStr in tmpMetaPathStr]
+
+        metaPaths.append(tmpMetaPath)
+
+    return metaPaths
+
+
+# get the weight of a group of meta paths
+def weight(metaPaths, edgeTypeStrength, methodFlag=1):
+    weights = []
+    for metaPath in metaPaths:
+        length = len(metaPath)
+        strength = 1
+        for i in range(0, length):
+            strength *= edgeTypeStrength[abs(metaPath[i])]
+        if methodFlag == 1:
+            weights.append(math.exp(strength - length))
+        else:
+            weights.append(strength/length)
+    sumWeights = sum(weights)
+    tmpWeights = [weight/sumWeights for weight in weights]
+    return tmpWeights
+
 
 # loading remaining user rating record to evaluae the similarity method
-def loadRatingGroundTruth(currentUserId, movieSampleNum):
-
+def loadRatingGroundTruth():
     fin = open('dataset/users_rate_test.txt','r')
     userRecords = fin.readlines()
     fin.close()
 
-    sampledMovies = set()
-    currUserGroundTruth = []
+    userRateGroundTruth = dict()
     for userRecord in userRecords:
-        [userStr, movieStr, ratingStr] = userRecord.split(" ")
+        [userStr, movieStr, ratingStr] = userRecord.strip().split(" ")
         userId = int(userStr)
         movieId = int(movieStr)
-        if userId == currentUserId:
-            sampledMovies.add(movieId)
-            currUserGroundTruth.append((float(ratingStr), movieId))
+        if userId not in userRateGroundTruth:
+            seenMovies = set()
+            seenMovies.add(movieId)
+            highRatingMovies = set()
+            if float(ratingStr) > 3:
+                highRatingMovies.add(movieId)
+            userRateGroundTruth[userId] = [seenMovies, highRatingMovies]
+        else:
+            userRateGroundTruth[userId][0].add(movieId)
+            if float(ratingStr) > 3:
+                userRateGroundTruth[userId][1].add(movieId)
 
-    if len(sampledMovies) > movieSampleNum:
-        sampledMovies = set(random.sample(sampledMovies, movieSampleNum))
-
-    currUserFinalGroundTruth = []
-    for rating, movieId in currUserGroundTruth:
-        if movieId in sampledMovies:
-            currUserFinalGroundTruth.append((rating, movieId))
-
-    currUserFinalGroundTruth.sort(reverse=True)
-
-    return sampledMovies, currUserFinalGroundTruth
+    return userRateGroundTruth
 
 
-def getMovieRank(hin, userEntityInfo, sampledMovies, metaPath, simFunc):
-    '''
-	calculate the movie rank list
-	:param HIN: the heterogeneous information network
-	:param userEntity: the user entityInfo
-	:param metaPath: meta-path
-    :simFunc: the similarity function
-	:return: weighted signed relation between eEntity and tEntity
-	'''
+def getDstEntitiesInfo(hin, srcEntityInfo, metaPath):
+    hinEntities = hin['Entities']
+    hinEntityType = hin['EntityTypes']
 
-    # filter those unrated movie in this entity
-    userEntity = userEntityInfo.entity
-    ratedMovieId = set()
-    if 'movie' in userEntityInfo.outRelations:
-        ratedMovieId.update(set(userEntityInfo.outRelations['movie'].keys()))
-
-    # get the similarity value and rank them
-    movieRank = []
-    timeCost = 0
-    simCalCount = 0
-    movieEntityInfoIndexes = hin['EntityTypes']['movie']
-    for movieId in sampledMovies:
-        movieEntity = hin['Entities'][movieEntityInfoIndexes[movieId]].entity
-        if movieEntity.entityId not in ratedMovieId:
-            startTime = time.time()
-            sim = simFunc(hin, userEntity, movieEntity, metaPath)
-            endTime = time.time()
-            timeCost += endTime - startTime
-            simCalCount += 1
-            movieRank.append((sim, movieId))
-    movieRank.sort(reverse=True)
-
-    if simCalCount == 0:
-        timeCost = None
-    else:
-        timeCost /= simCalCount
-
-    return movieRank, timeCost
+    dstEntitiesInfo = set()
+    dstEntitiesInfo.add(srcEntityInfo)
+    metaPathLength = len(metaPath)
+    for i in range(1, metaPathLength):
+        tmpDstEntitiesInfo = set()
+        currEntityType = metaPath[i]
+        for entityInfo in dstEntitiesInfo:
+            tmpOutRelation = entityInfo[currEntityType]
+            for nextEntityId in tmpOutRelation.keys():
+                tmpDstEntitiesInfo.add(hinEntities[hinEntityType[nextEntityId]])
+        dstEntitiesInfo.clear()
+        dstEntitiesInfo = tmpEndEntityInfo.copy()
 
 
-def getMetrics(groundTruth, predictResult, k):
-    if k > len(groundTruth):
-        k = len(groundTruth)
-        print("Not enough candidate movies")
-        print("current candidate num:" + str(k))
+    return dstEntitiesInfo
 
-    movieIdSample1 = set([movieId for _, movieId in groundTruth])
-    movieIdSample2 = set([movieId for _, movieId in predictResult])
-    # process ground truth first
-    groundTruthDict = dict()
-    sigMoidRatingSum = 0
-    for rating, movieId in groundTruth:
-        sigmoidRating = 1/(1 + math.exp(-rating))
-        sigMoidRatingSum += sigmoidRating
-        groundTruthDict[movieId] = sigmoidRating
-    # normalization
-    for movieId in groundTruthDict:
-        groundTruthDict[movieId] /= sigMoidRatingSum
 
-    groundTruthTopK = groundTruth[:k]
-    groundTruthTopKSet = set()
-    for _, movieId in groundTruthTopK:
-        groundTruthTopKSet.add(movieId)
-    predictResultTopK = predictResult[:k]
+def getPrecisionAndRecall(groundTruth, predictResult):
+    hitResult = groundTruth.intersection(predictResult)
+    hit = len(hitResult)
+    return hit/len(predictResult), hit/len(groundTruth)
 
-    # calculate the precision
-    hitCount = 0
-    for sim, movieId in predictResultTopK:
-        if movieId in groundTruthTopKSet:
-            hitCount += 1
-    precision = hitCount/k
-
-    return precision
+def getF1Score(precision, recall, beta=0.5):
+    return (1+beta*beta)*precision*recall/(beta*beta*precision + recall)
 
 
 def main():
@@ -113,43 +136,73 @@ def main():
     f.close()
 
     K = 10
-    userSampleNum = 10
-    movieSampleNum = 50
+    metaPathK = 5
+    methods = ["SP", "S-M-S"]
+    print('k ', K)
 
-    print('k ', K, 'user ', userSampleNum, 'movie', movieSampleNum)
-    # Meta path
-    metaPath = ['user','movie','genre','movie']
+    # load meta info
+    edgeTypeDict, edgeTypeStrength = loadEdgeTypeStrength()
+    # load ground truth
+    userRateGroundTruth = loadRatingGroundTruth()
 
     hinEntities = HIN['Entities']
     userEntityIdDict = HIN['EntityTypes']['user']
     movieEntityIdDict = HIN['EntityTypes']['movie']
-    # generate sampled users
-    sampledUserEntityIdList = random.sample(list(userEntityIdDict.keys()), userSampleNum)
-    sampledUserEntityInfoList = [hinEntities[userEntityIdDict[userEntityId]] for userEntityId in sampledUserEntityIdList]
 
-    simFuncList = [getWsRel, getSignSim, getPathSim, getHeteSim]
-    simFuncNameList = ['WsRel', 'SignSim', 'PathSim', 'HeteSim', ]
-    for i in range(4):
-        finalPrecision = avgTimeCost = 0
-        for userId in sampledUserEntityIdList:
-            userEntityInfo = HIN['Entities'][HIN['EntityTypes']['user'][userId]]
-            # generate the sampled movies and get the ground truth result
-            sampledMovies, rateGroundTruth = loadRatingGroundTruth(userId, movieSampleNum)
-            movieRank, timeCost = getMovieRank(HIN, userEntityInfo, sampledMovies, metaPath, simFuncList[i])
-            if movieRank == [] and timeCost == None:
-                userSampleNum -= 1
-                continue
-            precision = getMetrics(rateGroundTruth, movieRank, K)
-            finalPrecision += precision
-            avgTimeCost += timeCost
-        if userSampleNum != 0:
-            finalPrecision /= userSampleNum
-            avgTimeCost /= userSampleNum
-            print()
-            print(simFuncNameList[i] + " Result:")
-            print("Precision -> %.4f; cost time -> %.10f" % (finalPrecision, avgTimeCost))
-        else:
-            print("Failed to select candidate movies to perform the evaluation.")
+    # input pairs for testing
+    fin = open('dataset/HeteRecomInputPairsPython.txt','r')
+    testPairsRecords = fin.readlines()
+    testPairsSize = len(testPairsRecords)
+    for method in methods:
+        sumLinkPrediction = sumLinkRecall = 0
+        sumRatePrediction = sumRateRecall = 0
+        for record in testPairsRecords:
+            [srcStr, dstStr] = record.strip().split('\t')
+            src = int(srcStr)
+            dst = int(dstStr)
+            metaPaths = loadTopKMetaPath(src, dst, method, metaPathK, edgeTypeDict)
+            metaPathsWeights = weight(metaPaths, edgeTypeStrength)
+            userEntityInfo = HIN['Entities'][HIN['EntityTypes']['user'][src]]
+            exclusionMovies = set(userEntityInfo.outRelations['movie'].keys())
+            movieScores = dict()
+            movieScores.clear()
+            for i in range(metaPathK):
+                currMetaPath = metaPaths[i]
+                currMetaPathWeight = metaPathsWeights[i]
+                movieEntitiesInfo = getDstEntitiesInfo(HIN, userEntityInfo, currMetaPath)
+                for movieEntityInfo in movieEntitiesInfo:
+
+                    currMovieId = movieEntityInfo.entity.entityId
+                    # exclude those directly connected movies in training set
+                    if currMovieId in exclusionMovies:
+                        continue
+                    heteSim = getHeteSim(HIN, userEntityInfo, movieEntityInfo, currMetaPathWeight)
+                    if currMovieId not in movieScores:
+                        movieScores[currMovieId] = currMetaPathWeight*heteSim
+                    else:
+                        movieScores[currMovieId] += currMetaPathWeight*heteSim
+
+            movieScoresList = [(score, movie) for movie, score in movieScores]
+            movieScoresList.sort(reverse=True)
+            topKResult = [movie for _, movie in movieScoresList[:K]]
+
+            linkPrediction, linkRecall = getPrecisionAndRecall(userRateGroundTruth[src][0], topKResult)
+            sumLinkPrediction += linkPrediction
+            sumLinkRecall += linkRecall
+            ratePrediction, rateRecall = getPrecisionAndRecall(userRateGroundTruth[src][1], topKResult)
+            sumRatePrediction += ratePrediction
+            sumRateRecall += rateRecall
+
+        avgLinkPrediction = sumLinkPrediction/testPairsSize
+        avgLinkRecall = sumLinkRecall/testPairsSize
+        avgRatePrediction = sumRatePrediction/testPairsSize
+        avgRateRecall = sumRateRecall/testPairsSize
+
+        print("method: " + method)
+        print("Link: precision -> %.5f; recall -> %.5f; F1-score -> %.5f" % (avgLinkPrediction, avgLinkRecall, getF1Score(avgLinkPrediction, avgLinkRecall)))
+        print("Rate: precision -> %.5f; recall -> %.5f; F1-score -> %.5f" % (avgRatePrediction, avgRateRecall, getF1Score(avgRatePrediction, avgRateRecall)))
+        print()
+
 
 
 if __name__ == '__main__':
